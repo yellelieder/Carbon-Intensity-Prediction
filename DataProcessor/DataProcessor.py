1d5e61841ff7477cbaee2d2be90b3ccb6d2f35d1
Yelle Lieder <yelle.lieder@stud.uni-due.de>
2021-07-03 10:48:24 +0200

    csv reader added

--- DataProcessor/DataProcessor.py
+++ DataProcessor/DataProcessor.py
@@ -1,3 +1,29 @@
-import pandas
-production = "Ressources\\rawDataProduction.csv"
-consumption = "statics\\Ressources\\rawDataConsumption.csv"
+import csv
+import pandas as pd
+from datetime import datetime
+import regex as re
+rawProduction = "Ressources\\rawDataProduction.csv"
+rawConsumption = "Ressources\\rawDataConsumption.csv"
+
+timeStamp = re.sub('[-:. ]', '_', str(datetime.now().strftime("%Y-%m-%d %H:%M")))
+
+df=pd.DataFrame(pd.read_csv(rawProduction))
+
+
+df.to_csv("Ressources\\Training Data Production\\trainingProduction_"+timeStamp+".csv")
+
+print(df.loc[1]+df.loc[2])
+#"Datum"
+# "Uhrzeit";
+# "Biomasse[MWh]";
+# "Wasserkraft[MWh]";
+# "Wind Offshore[MWh]";
+# "Wind Onshore[MWh]";
+# "Photovoltaik[MWh]";
+# "Sonstige Erneuerbare[MWh]";
+# "Kernenergie[MWh]";
+# "Braunkohle[MWh]";
+# "Steinkohle[MWh]";
+# "Erdgas[MWh]";
+# "Pumpspeicher[MWh]";
+# "Sonstige Konventionelle[MWh]"
