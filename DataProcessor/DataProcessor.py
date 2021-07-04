5d0b4b714478f45ba6e854f25aa061e317ce6c5b
Yelle Lieder <yelle.lieder@stud.uni-due.de>
2021-07-04 09:54:28 +0200

    production data clean up ready

--- DataProcessor/DataProcessor.py
+++ DataProcessor/DataProcessor.py
@@ -1,4 +1,3 @@
-import csv
 import pandas as pd
 from datetime import datetime
 import regex as re
@@ -9,22 +8,37 @@ consumptionTargetFolder="Ressources\\Training Data Consumption\\"
 
 timeStamp = re.sub('[-:. ]', '_', str(datetime.now().strftime("%Y-%m-%d %H:%M")))
 
-df=pd.read_csv(rawProduction)
-print(df.head())
-print(df.loc[3])
-#df.to_csv(productionTargetFolder+"trainingProduction_"+timeStamp+".csv")
+df=pd.read_csv(rawProduction, sep=";")
+d={"Date":"n","Renevables":"n"}
+cleanData=pd.DataFrame(columns=["Date","Renevables"])
+for i, row in df.iterrows():
+    date=re.sub("[.]","/", row[0])+" "+row[1]+":00"
+    dict={"Date":date, "Renevables":row[2]+row[3]+row[4]+row[5]+row[6]+row[7]}
+    cleanData= cleanData.append(dict, ignore_index=True)
+    print(cleanData)
+cleanData.to_csv(productionTargetFolder+"trainingProduction_"+timeStamp+".csv")
 
-#"Datum"
+# "Datum"
 # "Uhrzeit";
+
 # "Biomasse[MWh]";
 # "Wasserkraft[MWh]";
 # "Wind Offshore[MWh]";
 # "Wind Onshore[MWh]";
 # "Photovoltaik[MWh]";
 # "Sonstige Erneuerbare[MWh]";
+
 # "Kernenergie[MWh]";
 # "Braunkohle[MWh]";
 # "Steinkohle[MWh]";
 # "Erdgas[MWh]";
 # "Pumpspeicher[MWh]";
 # "Sonstige Konventionelle[MWh]"
+
+
+
+#Datum;
+# Uhrzeit;
+# Gesamt (Netzlast)[MWh];
+# Residuallast[MWh];
+# Pumpspeicher[MWh]
