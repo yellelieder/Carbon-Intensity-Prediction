cb7f9b07ce6eee12ff9fe24c9a38f56e396afdbb
Yelle Lieder <yelle.lieder@stud.uni-due.de>
2021-07-05 08:29:51 +0200

    data visualization

--- DataProcessor/DataProcessor.py
+++ DataProcessor/DataProcessor.py
@@ -2,27 +2,25 @@ import pandas as pd
 from datetime import datetime
 import regex as re
 import matplotlib.pyplot as plt
-rawProduction = "Ressources\\rawDataProduction.csv"
-rawConsumption = "Ressources\\rawDataConsumption.csv"
-productionTargetFolder="Ressources\\Training Data Production\\"
-consumptionTargetFolder="Ressources\\Training Data Consumption\\"
+RAW_PRODUCTION_FILE = "Ressources\\rawDataProduction.csv"
+RAW_CONSUMPTION_FILE = "Ressources\\rawDataConsumption.csv"
+TARGET_PRODUCTION_FOLDER="Ressources\\Training Data Production\\"
+TARGET_CONSUMPTION_FOLDER="Ressources\\Training Data Consumption\\"
 
 timeStamp = re.sub('[-:. ]', '_', str(datetime.now().strftime("%Y-%m-%d %H:%M")))
 
-df=pd.read_csv(rawProduction, sep=";")
+df=pd.read_csv(RAW_PRODUCTION_FILE, sep=";")
 cleanProdData=pd.DataFrame(columns=["Date","Renevables"])
 for i, row in df.iterrows():
     date=re.sub("[.]","/", row[0])+" "+row[1]+":00"
     dict={"Date":date, "Renevables":int(str(row[2]).replace(".",""))+int(str(row[3]).replace(".",""))+int(str(row[4]).replace(".",""))+int(str(row[5]).replace(".",""))+int(str(row[6]).replace(".",""))+int(str(row[7]).replace(".",""))}
     cleanProdData= cleanProdData.append(dict, ignore_index=True)
-cleanProdData.to_csv(productionTargetFolder+"trainingProduction_"+timeStamp+".csv")
-print("production data cleaned up and persisted!")
+cleanProdData.to_csv(TARGET_PRODUCTION_FOLDER+"trainingProduction_"+timeStamp+".csv")
 
-df=pd.read_csv(rawConsumption, sep=";")
+df=pd.read_csv(RAW_CONSUMPTION_FILE, sep=";")
 cleanConData=pd.DataFrame(columns=["Date","Consumption"])
 for i, row in df.iterrows():
     date=re.sub("[.]","/", row[0])+" "+row[1]+":00"
     dict={"Date":date, "Consumption":int(str(row[2]).replace(".",""))}
     cleanConData= cleanConData.append(dict, ignore_index=True)
-cleanConData.to_csv(consumptionTargetFolder+"trainingConsumption_"+timeStamp+".csv")
-print("consumption data cleaned up and persisted!")
+cleanConData.to_csv(TARGET_CONSUMPTION_FOLDER+"trainingConsumption_"+timeStamp+".csv")
--- TrainingService/AutoRegression.py
+++ TrainingService/AutoRegression.py
@@ -1,4 +1,3 @@
-from posix import listdir
 import pandas as pd
 from datetime import datetime
 import regex as re
@@ -7,13 +6,21 @@ import os
 CONSUMPTION_FOLDER_PATH="Ressources\\Training Data Consumption\\"
 PRODUCTION_FOLDER_PATH="Ressources\\Training Data Production\\"
 
-def listDir(dir):
-    fileNames=os.listdir(dir)
-    fileNames=fileNames.sort()
-    return fileNames[-1]
+def getLatestFile(dir):
+    return sorted(os.listdir(dir)).pop()
+
+def parser(s):
+    return datetime.strftime(s,"%d/%m/%Y %H:%M:%S")
 
 if __name__ == "__main__":
-    listDir(CONSUMPTION_FOLDER_PATH)
-    listDir(PRODUCTION_FOLDER_PATH)
-
-df=pd.read_csv(CONSUMPTION_FOLDER_PATH+listdir(CONSUMPTION_FOLDER_PATH), sep=",")
+    df_production=pd.read_csv(PRODUCTION_FOLDER_PATH+getLatestFile(PRODUCTION_FOLDER_PATH), index_col=0, parse_dates=[1],sep=",")
+    plt.plot(df_production["Date"],df_production["Renevables"])
+    plt.ylabel("Production")
+    plt.xlabel("Time")
+    plt.show()
+    print(df_production)
+    df_consumption=pd.read_csv(CONSUMPTION_FOLDER_PATH+getLatestFile(CONSUMPTION_FOLDER_PATH), index_col=0, parse_dates=[1],sep=",")
+    plt.plot(df_consumption["Date"], df_consumption["Consumption"])
+    plt.ylabel("Consumption")
+    plt.show()
+    print(df_consumption)
