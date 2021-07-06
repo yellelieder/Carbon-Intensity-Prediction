0ba7103e0988cded85585a63ae716d54fff7e334
Yelle Lieder <yelle.lieder@stud.uni-due.de>
2021-07-06 08:05:28 +0200

    i build a maschine learning model, it is dumb af

--- TrainingService/AutoRegression.py
+++ TrainingService/AutoRegression.py
@@ -3,6 +3,12 @@ from datetime import datetime
 import regex as re
 import matplotlib.pyplot as plt
 import os
+from statsmodels.tsa.stattools import adfuller
+from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
+from statsmodels.tsa.ar_model import AutoReg
+from sklearn.metrics import mean_squared_error
+from math import sqrt
+
 CONSUMPTION_FOLDER_PATH="Ressources\\Training Data Consumption\\"
 PRODUCTION_FOLDER_PATH="Ressources\\Training Data Production\\"
 
@@ -14,11 +20,49 @@ def parser(s):
 
 if __name__ == "__main__":
     df_production=pd.read_csv(PRODUCTION_FOLDER_PATH+getLatestFile(PRODUCTION_FOLDER_PATH), index_col=0, parse_dates=[1],sep=",")
+    #plotting the production
     plt.plot(df_production["Date"],df_production["Renevables"])
     plt.ylabel("Production")
     plt.xlabel("Time")
     plt.show()
+    #checking that autoregression can be done on this data set, due to statistical limitations
+    dftest=adfuller(df_production["Renevables"], autolag="AIC")
+    print("1. ADF: ", dftest[0])
+    #if p is high (>0.5), timeseries is not stationary, but it should be
+    print("2. P-Value: ", dftest[1])
+    print("3. Num Of Lags: ", dftest[2])
+    print("4. Number Of Observations Used For ADF Regression and Critical Values Calculation: ", dftest[3])
+    print("5. Critical Values: ")
+    for key, val in dftest[4].items():
+        print("\t", key, ": ",val)
     print(df_production)
+    #check how many past values should be concidered
+    #partial autocorrelation
+    #shows how correlated different timeperiod values are
+    #contains only direct effects
+    pacf=plot_pacf(df_production["Renevables"], lags=1344)
+    #x axis shows how many periods (15 min) the correlation is back, 96 periods is one day, whole df has 525.600 periods
+    plt.show()
+    #1344 is for the past two weeks
+    pacf=plot_acf(df_production["Renevables"], lags=1344)
+    plt.show()
+    #define training set
+    x=df_production["Renevables"]
+    print(x)
+    no_of_days_to_predict = 14
+    train=x[:len(x)-no_of_days_to_predict]
+    test=x[len(x)-no_of_days_to_predict:]
+    model=AutoReg(train, lags=192).fit()
+    #p should be lower than 0.5
+    print(model.summary())
+    pred=model.predict(start=len(train), end=len(x)-1, dynamic=False)
+    plt.plot(pred)
+    plt.plot(test, color="red")
+    plt.show()
+    rmse=sqrt(mean_squared_error(test,pred))
+    print(rmse)
+
+
     df_consumption=pd.read_csv(CONSUMPTION_FOLDER_PATH+getLatestFile(CONSUMPTION_FOLDER_PATH), index_col=0, parse_dates=[1],sep=",")
     plt.plot(df_consumption["Date"], df_consumption["Consumption"])
     plt.ylabel("Consumption")
